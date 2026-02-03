31e3acf01e6b78f3e1116ce9a5c81720f6eeec05
YehchanP <yehchan.p@gmail.com>
2026-02-03 01:31:57 -0500

    Added delete functionality to admin blog page

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -2,6 +2,7 @@
 import BlogTableItem from '@/Components/AdminComponents/BlogTableItem'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
 
 const page = () => {
 
@@ -10,6 +11,12 @@ const page = () => {
     const fetchBlogs = async () => {
         const response = await axios.get('/api/blog');
         setBlogs(response.data.blogs);
+    }
+
+    const deleteBlog = async (mongoId) => {
+        const response = await axios.delete(`/api/blog?id=${mongoId}`);
+        toast.success(response.data.msg);
+        fetchBlogs();
     }
 
     useEffect(()=>{
@@ -39,7 +46,7 @@ const page = () => {
                 </thead>
                 <tbody>
                     {blogs.map((item,index)=>{
-                        return <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date}/>
+                        return <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date} deleteBlog={deleteBlog} />
                     })}
                 </tbody>
             </table>
--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -2,6 +2,7 @@ import { ConnectDB } from "@/lib/config/db"
 import BlogModel from "@/lib/models/BlogModel";
 const { NextResponse } = require("next/server")
 import {writeFile} from 'fs/promises'
+const fs = require('fs')
 
 const LoadDB = async () =>{
     await ConnectDB();
@@ -48,4 +49,13 @@ export async function POST(request) {
     console.log("Blog Saved");
 
     return NextResponse.json({success:true,msg:"Blog Added Successfully"});
+}
+
+// Create API Endpoint to delete blog
+export async function DELETE(request){
+    const id = await request.nextUrl.searchParams.get('id');
+    const blog = await BlogModel.findById(id);
+    fs.unlink(`./public${blog.image}`,()=>{});
+    await BlogModel.findByIdAndDelete(id);
+    return NextResponse.json({msg:"Blog Deleted"});
 }
