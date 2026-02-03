ce86fe5477643f47867fb01935f6ef0d60101003
YehchanP <yehchan.p@gmail.com>
2026-02-03 01:03:45 -0500

    Added Admin Bloglist page

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -11,8 +11,15 @@ LoadDB();
 
 // API Endpoint to get all blogs
 export async function GET(request){
+
+    const blogId = request.nextUrl.searchParams.get("id");
+    if(blogId){
+        const blog = await BlogModel.findById(blogId);
+        return NextResponse.json(blog);
+    }else{
     const blogs = await BlogModel.find({});
-    return NextResponse.json({blogs})
+    return NextResponse.json({blogs});
+    }
 }
 
 
